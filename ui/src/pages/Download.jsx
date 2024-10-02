4540fa3d384b0332690d66b02ead0c3394064460
phuongnam15 <ppnam150399@gmail.com>
2024-10-02 22:38:07 +0700

    update

--- ui/src/pages/Download.jsx
+++ ui/src/pages/Download.jsx
@@ -35,68 +35,69 @@ const Download = () => {
   }, [logs]);
 
   return (
-    <div className="container mx-auto h-screen p-4">
+    <div className="flex flex-col justify-center px-10 mx-auto h-screen dark:bg-gray-900 dark:text-white">
       <div className="mb-4 flex items-center justify-between">
         <div className="flex gap-2">
           <button
-            className="rounded bg-red-600 px-4 py-2 font-medium text-white"
+            className="rounded bg-red-600 px-4 py-1.5 font-medium text-white dark:bg-red-500 dark:hover:bg-red-400"
             onClick={handleStopProcess}
           >
             Stop
           </button>
           <button
-            className="rounded bg-green-600 px-4 py-2 font-medium text-white"
+            className="rounded bg-green-600 px-4 py-1.5 font-medium text-white dark:bg-green-500 dark:hover:bg-green-400"
             onClick={handleStart}
           >
             Start
           </button>
         </div>
-        <button className="bg-blue-500 text-white rounded py-2 px-4 text-md" onClick={() => navigate("/up-live")}>Up Live</button>
+        <button
+          className="text-md rounded bg-blue-500 px-4 py-1.5 text-white dark:bg-blue-500 dark:hover:bg-blue-400"
+          onClick={() => navigate("/up-live")}
+        >
+          Up Live
+        </button>
       </div>
 
-      <div className="rounded border p-4 shadow">
-        {/* input for cookie */}
-        <div className="mb-4">
-          <label htmlFor="cookie" className="mb-2 block font-bold">
-            Cookie
-          </label>
+      <div className="rounded border p-4 shadow dark:border-gray-700 dark:bg-gray-800">
+        <div className="mb-2">
           <input
             type="text"
             id="cookie"
             value={cookie}
             onChange={(e) => handleCookieChange(e.target.value)}
-            className="mb-2 w-full border p-2"
+            className="w-full border px-2 py-1 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200 outline-none"
+            placeholder="cookie"
           />
         </div>
 
-        {/* input for username */}
-        <div className="mb-4">
-          <label htmlFor="username" className="mb-2 block font-bold">
-            TikTok Username
-          </label>
+        <div className="mb-2">
           <input
             type="text"
             id="username"
             value={username}
             onChange={(e) => handleTiktokInfoChange(e.target.value)}
-            className="mb-2 w-full border p-2"
+            className="w-full border px-2 py-1 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200 outline-none"
+            placeholder="username"
           />
         </div>
 
-        {/* show logs */}
-        <div className="h-96 overflow-y-auto bg-gray-100 p-2">
-          <h3 className="mb-2 font-bold">Logs:</h3>
+        {/* Show logs */}
+        <div className="h-[26rem] overflow-y-auto bg-gray-100 p-2 dark:bg-gray-700">
+          <h3 className="mb-2 font-bold dark:text-gray-300">Logs:</h3>
           {logs.length > 0 ? (
             <ul>
               {logs.map((log, i) => (
-                <li key={i} className="text-sm">
+                <li key={i} className="text-sm dark:text-gray-200">
                   {log}
                 </li>
               ))}
               <div ref={logsEndRef} />
             </ul>
           ) : (
-            <p className="text-sm text-gray-500">No logs yet.</p>
+            <p className="text-sm text-gray-500 dark:text-gray-400">
+              No logs yet.
+            </p>
           )}
         </div>
       </div>
--- ui/src/pages/UpLive.jsx
+++ ui/src/pages/UpLive.jsx
@@ -9,6 +9,9 @@ const UpLive = () => {
     { key_live: "", file: null },
   ]);
   const [responseData, setResponseData] = useState([]);
+  const isStopButtonEnabled =
+    responseData.length > 0 &&
+    responseData.some((item) => item.status === "success");
 
   const handleNumWindowsChange = (e) => {
     const newNumWindows = parseInt(e.target.value, 10);
@@ -84,16 +87,14 @@ const UpLive = () => {
   };
 
   return (
-    <div className="container mx-auto flex flex-col justify-center p-4">
+    <div className="mx-auto flex h-screen flex-col px-8 py-4 dark:bg-gray-900 dark:text-white">
+      {/* Section for number of windows selection */}
       <div className="mb-4 flex items-center gap-3">
-        <label htmlFor="numWindows" className="text-md font-medium">
-          Select Number of Windows
-        </label>
         <select
           id="numWindows"
           value={numWindows}
           onChange={handleNumWindowsChange}
-          className="rounded border p-1"
+          className="rounded border p-1 outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-white"
         >
           {Array.from({ length: 10 }, (_, i) => (
             <option key={i + 1} value={i + 1}>
@@ -101,36 +102,52 @@ const UpLive = () => {
             </option>
           ))}
         </select>
+        <label
+          htmlFor="numWindows"
+          className="text-md font-medium dark:text-gray-300"
+        >
+          windows
+        </label>
       </div>
 
-      <div className="mb-2 flex gap-1">
+      {/* Buttons section */}
+      <div className="mb-4 flex gap-1.5">
         <button
           onClick={handleUpload}
-          className="rounded bg-blue-600 px-3 py-2 text-white shadow transition-colors hover:bg-blue-700"
+          className="rounded bg-blue-500 px-3 py-1.5 text-white shadow transition-colors hover:bg-blue-400 dark:bg-blue-500 dark:hover:bg-blue-400"
         >
           Upload
         </button>
         <button
-          className="rounded bg-red-500 px-3 py-2 text-white"
+          className={`rounded bg-red-500 px-3 py-1.5 text-white ${
+            isStopButtonEnabled
+              ? "dark:bg-red-500 dark:hover:bg-red-400"
+              : "cursor-not-allowed opacity-50"
+          }`}
           onClick={handleStopAll}
+          disabled={!isStopButtonEnabled}
         >
           Stop All
         </button>
         <button
-          className="rounded bg-teal-500 px-3 py-2 text-white"
+          className="rounded bg-teal-500 px-3 py-1.5 text-white dark:bg-teal-500 dark:hover:bg-teal-400"
           onClick={() => navigate("/")}
         >
           Download
         </button>
       </div>
 
+      {/* Live Streams Inputs Section */}
       <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
         {liveStreams.map((stream, index) => (
-          <div key={index} className="rounded border p-2 border-gray-400">
+          <div
+            key={index}
+            className="rounded border border-gray-400 p-2 dark:border-gray-600 dark:bg-gray-800"
+          >
             <div className="mb-2">
               <label
                 htmlFor={`keyLive-${index}`}
-                className="mb-2 block text-sm font-bold"
+                className="mb-2 block text-sm font-bold dark:text-gray-300"
               >
                 Key Live {index + 1}
               </label>
@@ -139,7 +156,7 @@ const UpLive = () => {
                 id={`keyLive-${index}`}
                 value={stream.key_live}
                 onChange={(e) => handleKeyLiveChange(index, e.target.value)}
-                className="w-full rounded border p-2 text-sm border-gray-400"
+                className="w-full rounded border border-gray-400 p-2 text-sm outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200"
                 placeholder={`Enter your key live for window ${index + 1}`}
               />
             </div>
@@ -147,7 +164,7 @@ const UpLive = () => {
             <div className="mb-2">
               <label
                 htmlFor={`fileUpload-${index}`}
-                className="mb-2 block text-sm font-bold"
+                className="mb-2 block text-sm font-bold dark:text-gray-300"
               >
                 Select File {index + 1}
               </label>
@@ -155,20 +172,23 @@ const UpLive = () => {
                 type="file"
                 id={`fileUpload-${index}`}
                 onChange={(e) => handleFileChange(index, e.target.files[0])}
-                className="w-full rounded border p-2 border-gray-400"
+                className="w-full rounded border border-gray-400 p-2 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200"
               />
             </div>
 
+            {/* Response Data Section */}
             {responseData.length > 0 &&
               (responseData[index]?.status === "success" ? (
                 <button
                   onClick={() => handleStop([responseData[index].streamId])}
-                  className="mt-2 rounded bg-red-600 px-3 py-1 text-white"
+                  className="mt-2 rounded bg-red-600 px-3 py-1 text-white dark:bg-red-500 dark:hover:bg-red-600"
                 >
                   Stop
                 </button>
               ) : (
-                <span>{responseData[index]?.msg}</span>
+                <span className="dark:text-gray-400">
+                  {responseData[index]?.msg}
+                </span>
               ))}
           </div>
         ))}
